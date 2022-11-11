108141fa20c3f30ea0cf7b17db39776735520fcd
Apeiron2k3 <90538612+Apeiron2k3@users.noreply.github.com>
2022-11-11 16:53:59 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function check(){
+    document.getElementById("No").innerHTML="Có";
+    document.getElementById("No").style.backgroundColor="pink";
+    document.getElementById("No").style.color="black";
+    setTimeout(
+        () => {
+            document.write("Em cũng yêu anh <3");
+        },
+        1 * 1000
+      );
+}
